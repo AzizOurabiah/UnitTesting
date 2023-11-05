81c7884d49cf7853cbcc49daa4fab244a4f52760
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2023-11-05 09:05:54 -0500

    Ajout les ligne de test dans les méthode DataRow

--- BankAccountLib.MsTest.UnitTests/BankAccountLibUnitTest.cs
+++ BankAccountLib.MsTest.UnitTests/BankAccountLibUnitTest.cs
@@ -5,33 +5,39 @@ namespace BankAccountLib.MsTest.UnitTests
     {
         [TestMethod]
         [TestCategory("Credit")]
-        public void Credit_ValidAmount_IncrementBalance()
+        [DataRow(1000, 100, 1100, 1100)]
+        [DataRow(2000, 100, 2100, 2100)]
+        [DataRow(3000, 100, 3100, 3100)]
+        public void Credit_ValidAmount_IncrementBalance(double amount, double credit, double expected, double actual)
         {
             //Arrange
-            BankAccount bankAccount = new BankAccount("Adam", 1000);
+            BankAccount bankAccount = new BankAccount("Adam", amount);
 
             //Act
-            bankAccount.Credit(100);
-            var real = bankAccount.Balance;
-            var expected = 1100;
+            bankAccount.Credit(credit);
+            //var real = bankAccount.Balance;
+            //var expected = 1100;
 
             //Assert
-            Assert.AreEqual(expected, real);
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
         [TestCategory("Debit")]
-        public void Debit_ValidAmount_DecrementBalance()
+        [DataRow(1000, 100, 900, 900)]
+        [DataRow(2000, 100, 1900, 1900)]
+        [DataRow(3000, 100, 2900, 2900)]
+        public void Debit_ValidAmount_DecrementBalance(double amount, double debit, double expected, double actual)
         {
             //Arrange
-            BankAccount bankAccount = new BankAccount("Adam", 1000);
+            BankAccount bankAccount = new BankAccount("Adam", amount);
 
             //Act
-            bankAccount.Debit(100);
-            var real = bankAccount.Balance;
-            var expected = 900;
+            bankAccount.Debit(debit);
+            //var real = bankAccount.Balance;
+            //var expected = 900;
 
             //Assert
-            Assert.AreEqual(expected, real);
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
         [TestCategory("Debit")]
@@ -58,21 +64,27 @@ namespace BankAccountLib.MsTest.UnitTests
         }
         [TestMethod]
         [TestCategory("Debit")]
-        public void Debit_AmountBiggerThenBalance_ThrowException2()
+        [DataRow(1000, 2000)]
+        [DataRow(1000, 3000)]
+        [DataRow(1000, 4000)]
+        public void Debit_AmountBiggerThenBalance_ThrowException2(double amount, double debit)
         {
             //Arrange
-            var sut = new BankAccount("Adam", 1000);
+            var sut = new BankAccount("Adam", amount);
 
             //Act
             //Assert
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => sut.Debit(2000));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => sut.Debit(debit));
         }
         [TestMethod]
         [TestCategory("Credit")]
-        public void Credit_MaxAmount_ThrowException2()
+        [DataRow(1000)]
+        [DataRow(2000)]
+        [DataRow(3000)]
+        public void Credit_MaxAmount_ThrowException2(double amount)
         {
             //Arrange
-            var sut = new BankAccount("Adam", 1000);
+            var sut = new BankAccount("Adam", amount);
 
             //Act
             //Assert
